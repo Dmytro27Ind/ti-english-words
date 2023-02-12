7915256e6a0ebe95bdfe810dd0d791b99eca2fca
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2023-02-12 23:08:08 +0100

    Custom themes added

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,4 +1,23 @@
 
+//  ____________________  1  ______________________
+// $.navWin.open().then(() => {
+//     $.tabGroup.open().then((result) => {
+//         Ti.API.debug("___________________________________")
+//         Ti.API.debug(result)
+//         Ti.API.debug($.tabGroup)
+//         Ti.API.debug("___________________________________")
+
+//         Ti.API.debug($.tabGroup.theme)
+//         // $.tabGroup.theme = 'Theme.MyDarkTheme'
+//         $.tabGroup.applyProperties({theme: 'Theme.MyDarkTheme'})
+//         Ti.API.debug($.tabGroup.theme)
+//         // $.tabGroup.close().then(() => {
+//         //     $.tabGroup.open()
+//         // })
+//     })
+// })
+
+//  ____________________  2  ______________________
 // let listWords = [ "example", "audio", "get", "definition", "send", "request" ]
 
 // $.win.open().then(() => {
@@ -11,4 +30,14 @@
 // 	Alloy.Globals.startRequest("https://api.dictionaryapi.dev/api/v2/entries/en/hello", $.win.activity)
 // }
 
+
+//  ____________________  3  ______________________
+// $.win.open()
+// function setDark() {
+//     Ti.API.debug($.win.theme)
+//     $.win.applyProperties({theme: 'Theme.MyDarkTheme'})
+//     Ti.API.debug($.win.theme)
+// }
+
+//  ____________________  4  ______________________
 $.tabGroup.open()
